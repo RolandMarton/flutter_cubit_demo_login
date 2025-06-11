383db39c3abd805d00b0ae8c5f59c47df5d12403
RolandMarton <roland111187@gmail.com>
2025-06-11 11:41:24 +0200

    Change the basic structure for listener

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -3,6 +3,7 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 
 import 'package:login_cubit_test/cubit/user_cubit.dart';
 import 'package:login_cubit_test/cubit/theme_cubit.dart';
+import 'package:login_cubit_test/models/user.dart';
 
 import 'package:login_cubit_test/repository/auth_repository.dart';
 
@@ -15,32 +16,38 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     final user = context.watch<UserCubit>().state;
 
-    return Scaffold(
-      appBar: AppBar(title: const Text('Home')),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            if (user != null) Text('Welcome, ${user.username} (${user.email})'),
-            const SizedBox(height: 20),
-            ElevatedButton(
-              onPressed: () async {
-                await context.read<AuthRepository>().logout();
+    return BlocListener<UserCubit, User?>(
+      listener: (context, user) {
+        if (user == null && context.mounted) {
+          Navigator.pushReplacementNamed(context, '/');
+        }
+      },
+      child: Scaffold(
+        appBar: AppBar(title: const Text('Home')),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              if (user != null) Text('Welcome, ${user.username} (${user.email})'),
+              const SizedBox(height: 20),
+              ElevatedButton(
+                onPressed: () async {
+                  await context.read<AuthRepository>().logout();
 
-                if (context.mounted) {
-                  context.read<AnalyticsService>().logEvent('logout');
-                  context.read<UserCubit>().clearUser();
-                  Navigator.pushReplacementNamed(context, '/');
-                }
-              },
-              child: const Text('Logout'),
-            ),
-            const SizedBox(height: 20),
-            ElevatedButton(
-              onPressed: () => context.read<ThemeCubit>().toggleTheme(),
-              child: const Text('Toggle Theme'),
-            ),
-          ],
+                  if (context.mounted) {
+                    context.read<AnalyticsService>().logEvent('logout');
+                    context.read<UserCubit>().clearUser();
+                  }
+                },
+                child: const Text('Logout'),
+              ),
+              const SizedBox(height: 20),
+              ElevatedButton(
+                onPressed: () => context.read<ThemeCubit>().toggleTheme(),
+                child: const Text('Toggle Theme'),
+              ),
+            ],
+          ),
         ),
       ),
     );
